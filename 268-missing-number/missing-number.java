fabfd4276b0f33c775a31f5650ea146e5d03ddef
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-07-29 15:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (16.63%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,29 @@
 class Solution {
+    // solution with sum approach
+    // public int missingNumber(int[] nums) {
+    //     int n = nums.length;
+    //     int sumOfNums = 0;
+    //     for(int i=0; i<n; i++) {
+    //         sumOfNums += nums[i];
+    //     }
+    //     int sumOfNaturalNums = n*(n+1)/2;
+    //     return sumOfNaturalNums - sumOfNums;
+    // }
+
+    // solution with xor approach
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int sumOfNums = 0;
-        for(int i=0; i<n; i++) {
-            sumOfNums += nums[i];
+        int xor1 = 0;
+        int xor2 = 0;
+
+        for(int i=0; i<=n; i++) {
+            xor1 = xor1 ^ i;
         }
-        int sumOfNaturalNums = n*(n+1)/2;
-        return sumOfNaturalNums - sumOfNums;
+        
+        for(int i=0; i<n; i++) {
+            xor2 = xor2 ^ nums[i];
+        }
+
+        return xor1 ^ xor2;
     }
 }
