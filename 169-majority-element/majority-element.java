00d8ea2ac4b273e589a7e98276daed4eb6de3d47
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-07-31 13:37:29 +0530

    Time: 2 ms (75.38%) | Memory: 53.3 MB (11.49%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,42 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityEle = nums[0];
-        HashMap<Integer, Integer> freq = new HashMap<>();
+        // storing the frequency in hashmap approach
+        // Time Complexity: O(n)
+        // int majorityEle = nums[0];
+        // HashMap<Integer, Integer> freq = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(freq.containsKey(nums[i])) {
+        //         freq.put(nums[i], freq.get(nums[i]) + 1);
+        //     } else {
+        //         freq.put(nums[i], 1);
+        //     }
+        // }
+        // for (Map.Entry<Integer, Integer> set : freq.entrySet()) {
+        //     int n = nums.length;
+        //     if(set.getValue() > n/2) {
+        //         if(set.getValue() > freq.get(majorityEle)) {
+        //             majorityEle = set.getKey();
+        //         }
+        //     }
+        // }
+        // return majorityEle;
+
+        // Optimal Approach: Moore's Voting Algorithm
+        int element = nums[0];
+        int count = 0;
         for(int i = 0; i < nums.length; i++) {
-            if(freq.containsKey(nums[i])) {
-                freq.put(nums[i], freq.get(nums[i]) + 1);
+            if(count == 0) {
+                element = nums[i];
+                count++;
             } else {
-                freq.put(nums[i], 1);
-            }
-        }
-        for (Map.Entry<Integer, Integer> set : freq.entrySet()) {
-            int n = nums.length;
-            if(set.getValue() > n/2) {
-                if(set.getValue() > freq.get(majorityEle)) {
-                    majorityEle = set.getKey();
+                if(nums[i] == element) {
+                    count++;
+                } else {
+                    count--;
                 }
             }
         }
-        return majorityEle;
+
+        return element;
     }
 }
