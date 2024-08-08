98bce9ccce3b7e088007f0bdff889f2963cf1cd4
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-08 15:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (72.92%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -4,7 +4,23 @@ class Solution {
         for(int i = 0; i < n; i++) {
             nums1[m+i] = nums2[i];
         }
-        
-        Arrays.sort(nums1);
+        // using gap method
+        int len = m + n;
+        int gap = (len/2) + (len%2);
+        while(gap > 0) {
+            int left = 0;
+            int right = left + gap;
+            while(right < len) {
+                if(nums1[left] > nums1[right]) {
+                    int temp = nums1[left];
+                    nums1[left] = nums1[right];
+                    nums1[right] = temp;
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
     }
 }
