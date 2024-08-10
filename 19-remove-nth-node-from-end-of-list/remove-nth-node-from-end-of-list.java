f69479b17e7f8e811a497994ecaa152e37c0d25f
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-10 20:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (7.41%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -9,33 +9,58 @@
  * }
  */
 class Solution {
+    // This solution has O(N) + O(N) = O(2N) time complexity
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     int size = 0, index = 0;
+    //     // calculate the size of the linked list
+    //     ListNode curr = head;
+    //     while(curr != null) {
+    //         size++;
+    //         curr = curr.next;
+    //     }
+    //     // calculate the index of the node that has to be removed
+    //     index = (size - n);
+    //     // remove the node
+    //     int counter = 0;
+    //     curr = head;
+    //     while(curr != null) {
+    //         counter++;
+    //         // edge case if we need to remove first node
+    //         if(n == size) {
+    //             head = head.next;
+    //             break;
+    //         }
+    //         // one node before the node that has to be removed
+    //         if(index == counter) {
+    //             curr.next = curr.next.next;
+    //             break;
+    //         }
+    //         curr = curr.next;
+    //     }
+    //     return head;
+    // }
+
+    // Optimal Approach: Fast Pointer, Slow Pointer
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int size = 0, index = 0;
-        // calculate the size of the linked list
-        ListNode curr = head;
-        while(curr != null) {
-            size++;
-            curr = curr.next;
+        // The idea is to first move fast pointer until it reaches n
+        // Then slow pointer will start from the head
+        // The loop will go on until fast pointer comes to the last node
+        // Then we just have to remove the next of slow pointer
+        
+        ListNode fast = head, slow = head;
+        for(int i = 0; i< n; i++) {
+            fast = fast.next;
         }
-        // calculate the index of the node that has to be removed
-        index = (size - n);
-        // remove the node
-        int counter = 0;
-        curr = head;
-        while(curr != null) {
-            counter++;
-            // edge case if we need to remove first node
-            if(n == size) {
-                head = head.next;
-                break;
-            }
-            // one node before the node that has to be removed
-            if(index == counter) {
-                curr.next = curr.next.next;
-                break;
-            }
-            curr = curr.next;
+        // if fast becomes null then then element must next of head
+        if(fast == null)
+            return head.next;
+        
+        while(fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
+        slow.next = slow.next.next;
         return head;
+
     }
 }
