01d4bb8ba3072920e1478935f33d4d17defb05fd
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-10 16:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (67.53%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -9,23 +9,36 @@
  * }
  */
 class Solution {
+    // This brute force approach has time complexity of O(N)+O(N/2) and O(1) space complexity
+    // public ListNode middleNode(ListNode head) {
+    //     int size = 0;
+    //     ListNode temp = head;
+    //     while(temp != null) {
+    //         size++;
+    //         temp = temp.next;
+    //     }
+    //     temp = head;
+    //     int midPos = (size / 2) + 1;
+    //     int counter = 0;
+    //     while(temp != null) {
+    //         counter++;
+    //         if(counter == midPos) {
+    //             break;
+    //         }
+    //         temp = temp.next;
+    //     }
+    //     return temp;
+    // }
+
+    // The fast pointer and slow pointer approach
+    // Time Complexity: O(N/2)
+    // We just need to loop until the fast pointer reaches or goes beyond linkedlist's end
     public ListNode middleNode(ListNode head) {
-        int size = 0;
-        ListNode temp = head;
-        while(temp != null) {
-            size++;
-            temp = temp.next;
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        int midPos = (size / 2) + 1;
-        int counter = 0;
-        while(temp != null) {
-            counter++;
-            if(counter == midPos) {
-                break;
-            }
-            temp = temp.next;
-        }
-        return temp;
+        return slow;
     }
 }
