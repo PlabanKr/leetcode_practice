4bfd3ee3d30fbc33654a47aff9cc6328b6b9d6fa
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-14 13:57:23 +0530

    Time: 1 ms (19.19%) | Memory: 44.2 MB (75.52%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        // If there is no cycle then fast pointer's next will be null eventually
+        // If we had used 'fast != null' in while statement then at the head node where both pointers starts
+        // and point to the same node would have triggered line 19
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast.equals(slow)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
