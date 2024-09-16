1a05ea17d60988360ba986729044c9b5ee0f70f8
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-09-16 11:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (99.53%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,31 +14,34 @@
  * }
  */
 class Solution {
-    // public int maxDepth(TreeNode root) {
-    //     if(root == null) return 0;
-    //     int lh = maxDepth(root.left);
-    //     int rh = maxDepth(root.right);
-    //     return 1 + Math.max(lh, rh);
-    // }
+    // RECURSIVE METHOD
 
     public int maxDepth(TreeNode root) {
-        int depth = 0;
-        if(root == null) return depth;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            int level = queue.size();
-            for(int i = 0; i < level; i++) {
-                if(queue.peek().left != null)
-                    queue.offer(queue.peek().left);
-                if(queue.peek().right != null)
-                    queue.offer(queue.peek().right);
-                queue.poll();
-            }
-            depth++;
-        }
-        return depth;
+        if(root == null) return 0;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1 + Math.max(lh, rh);
     }
+
+    // LEVEL ORDER / ITERATIVE METHOD
+    // public int maxDepth(TreeNode root) {
+    //     int depth = 0;
+    //     if(root == null) return depth;
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
+    //     while(!queue.isEmpty()) {
+    //         int level = queue.size();
+    //         for(int i = 0; i < level; i++) {
+    //             if(queue.peek().left != null)
+    //                 queue.offer(queue.peek().left);
+    //             if(queue.peek().right != null)
+    //                 queue.offer(queue.peek().right);
+    //             queue.poll();
+    //         }
+    //         depth++;
+    //     }
+    //     return depth;
+    // }
 }
 
 
