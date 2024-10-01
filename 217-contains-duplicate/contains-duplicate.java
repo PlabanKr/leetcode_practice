072a6f95d3e99dd0911930ba18ae0f0f0da0f361
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-10-01 23:33:48 +0530

    Time: 11 ms (72.31%) | Memory: 57.7 MB (61.53%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,9 +1,24 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+    // public boolean containsDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-1; i++) {
-            if(nums[i] == nums[i+1]) {
+    //     for(int i=0; i<nums.length-1; i++) {
+    //         if(nums[i] == nums[i+1]) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // HASHMAP
+    public boolean containsDuplicate(int[] nums) {
+        // store frequency of a number
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!hm.containsKey(nums[i])) {
+                hm.put(nums[i], 1);
+            } else {
                 return true;
             }
         }
